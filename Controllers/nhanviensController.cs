f5d808356e947d1550a28c493f009ae1ac18cd07
nguyen van huy <huynvth1907054@fpt.edu.vn>
2020-10-30 16:25:25 +0700

    Nop Bai

---
+++ Controllers/nhanviensController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using BaiThi.Context;
+using BaiThi.Models;
+
+namespace BaiThi.Controllers
+{
+    public class nhanviensController : Controller
+    {
+        private nhanvienContext db = new nhanvienContext();
+
+        // GET: nhanviens
+        public ActionResult Index()
+        {
+            return View(db.nhanviens.ToList());
+        }
+
+        // GET: nhanviens/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            nhanvien nhanvien = db.nhanviens.Find(id);
+            if (nhanvien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhanvien);
+        }
+
+        // GET: nhanviens/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: nhanviens/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "EmployeeID,Employeename,Department,Salary")] nhanvien nhanvien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.nhanviens.Add(nhanvien);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(nhanvien);
+        }
+
+        // GET: nhanviens/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            nhanvien nhanvien = db.nhanviens.Find(id);
+            if (nhanvien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhanvien);
+        }
+
+        // POST: nhanviens/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "EmployeeID,Employeename,Department,Salary")] nhanvien nhanvien)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(nhanvien).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(nhanvien);
+        }
+
+        // GET: nhanviens/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            nhanvien nhanvien = db.nhanviens.Find(id);
+            if (nhanvien == null)
+            {
+                return HttpNotFound();
+            }
+            return View(nhanvien);
+        }
+
+        // POST: nhanviens/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            nhanvien nhanvien = db.nhanviens.Find(id);
+            db.nhanviens.Remove(nhanvien);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/nhanvien.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BaiThi.Models
+{
+    public class nhanvien
+    {
+        [Key]
+        [Required(ErrorMessage = "EmployeeID is required")]
+        [StringLength(1)]
+        public string EmployeeID { get; set; }
+        public string Employeename { get; set; }
+        public string Department { get; set; }
+        public int Salary { get; set; }
+        
+    }
+}
